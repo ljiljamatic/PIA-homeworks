91288bffbcf5cf6af088a306724171eb52c743f2
ljiljamatic <ljiljamatic99@gmail.com>
2020-12-23 01:37:27 +0100

    Fix the submit button

---
+++ hw-3/quiz.js
@@ -0,0 +1,59 @@
+//Start Section
+
+let start = document.querySelector("#start");
+let playerName = document.querySelector("#name");
+//Quide section
+
+let guide = document.querySelector("#guide");
+let exit = document.querySelector("#exit");
+let continueBtn = document.querySelector("#continue");
+
+//Quiz section
+
+let quiz = document.querySelector("#quiz");
+let time = document.querySelector("#time");
+
+//Question section
+let questionNo = document.querySelector("#questionNo");
+let questionText = document.querySelector("#questionText");
+
+//Answers
+let option1 = document.querySelector("#option1");
+let option2 = document.querySelector("#option2");
+let option3 = document.querySelector("#option3");
+let option4 = document.querySelector("#option4");
+
+//Score and next question
+let total_correct = document.querySelector("#total_correct");
+let next_question = document.querySelector("#next_question");
+
+//Result section
+let result = document.querySelector("#result");
+let points = document.querySelector("#points");
+let quit = document.querySelector("#quit");
+let startAgain = document.querySelector("#startAgain");
+
+//Choices 
+let choice_que = document.querySelector(".choice_que");
+
+let index = 0;
+let timer = 0;
+let interval = 0;
+
+//Score
+let correct = 0;
+
+//Answer
+let UserAns = undefined;
+
+//SUBMIT BUTTON
+start.addEventListener('click', ()=>{
+    start.style.display = "none";
+    playerName.style.display = "none";
+    guide.style.display = "block";
+    console.log(playerName.value);
+});
+
+
+
+
